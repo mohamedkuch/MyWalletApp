0f46c30c77d88ffee8625c237e73e67edc9758bc
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2019-12-22 02:26:46 +0100

    finish less 86

---
+++ expense_app/lib/widgets/new_transaction.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              // onChanged: (value) {
+              //   titleInput = value;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              // onChanged: (value) {
+              //   amountInput = value;
+              // },
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+              textColor: Colors.purple,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
